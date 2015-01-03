4c49d5927c63bfe5b9a8abd5c42bda604c54645a
Diljit <diljitpr@gmail.com>
2015-01-04 00:25:17 +0530

    added docstrings for function

--- misc/generatePermuation.py
+++ misc/generatePermuation.py
@@ -5,13 +5,28 @@
 	
 """
 def swap(string,index1,index2):
-	""""""
+	"""
+	Function swap takes three parameters(string,index,index2) and swaps the item at index with index2
+	
+	@param string type:list the string to be swapped
+	@Param index1 type:int the index to be swapped
+	@param index2 type:int the index to be swapped 
+	
+	"""
 	temp = string[index1]
 	string[index1] = string[index2]
 	string[index2] = temp
 
 def permute(string,start,end):
-	""""""
+	"""
+	This generates all the permutation of a given string
+	
+	@param string type:string  the string to be permuted
+	@param start: type:integer the initial index of the string i.e 0 in all cases
+	@param end:   type:integer the length of the string
+	
+	
+	"""
 	if start == end:
 		print ''.join(string)
 		
